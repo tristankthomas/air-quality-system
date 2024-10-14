4b878ce6fdef69e60b02e8a3dc01759ac02cf31b
Tristan Thomas <tristankthomas@icloud.com>
2024-10-15 10:57:24 +1100

    Added Twilio call when gas sensor is too high

--- server/gas.py
+++ server/gas.py
@@ -3,9 +3,18 @@ import serial
 import threading
 import json
 from fastapi.middleware.cors import CORSMiddleware
+from twilio.rest import Client
+import time
 
 app = FastAPI()
 device = "ttyUSB0"
+
+# Twilio setup
+account_sid = 'ACf1e2638d9b882481cb81b7b232300c16'
+auth_token = '72993798b297e6e1c33f2561331e153d'
+twilio_client = Client(account_sid, auth_token)
+twilio_phone_number = '+12029198556'
+recipient_phone_number = '+61490514927'
 
 origins = [
     "http://localhost:8080",  # Allow your Vue.js app running locally
@@ -35,9 +44,29 @@ def read_gas_sensor():
             line = ser.readline().decode('utf-8').strip()  # Read data from Arduino
             if line:
                 sensor_data = line  # Store the latest sensor data
+                check_gas_level(sensor_data)
         except serial.SerialException:
             print("Serial connection issue")
             break
+
+def check_gas_level(sensor_data):
+    try:
+        data_json = json.loads(sensor_data)
+        gas_level = int(data_json.get("gas", 0))  # Adjust this key to match your data structure
+        if gas_level > 800:
+            make_twilio_call()  # Trigger Twilio call
+            time.sleep(300)
+            print("Sleeping\n")
+    except json.JSONDecodeError:
+        print("Error decoding JSON from sensor")
+
+def make_twilio_call():
+    call = twilio_client.calls.create(
+        twiml='<Response><Say>Warning! Gas levels are too high. Please check immediately!</Say></Response>',
+        to=recipient_phone_number,
+        from_=twilio_phone_number
+    )
+    print(f"Call initiated: {call.sid}")
 
 # Start the thread for reading sensor data
 thread = threading.Thread(target=read_gas_sensor)
